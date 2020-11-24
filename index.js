8b26f1f743958be1e03b6b99e98468e0c41b89e5
mengdehua <mengdehuavipkid.com.cn@igetter.local>
2020-11-24 12:01:40 +0800

    0.0.1

---
+++ index.js
@@ -0,0 +1,21 @@
+var hljs = require('highlight.js');
+var marked = require('marked')
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: function(code, language) {
+    const validLanguage = hljs.getLanguage(language) ? language : 'html';
+    return hljs.highlight(validLanguage, code).value;
+  },
+  pedantic: false,
+  gfm: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+});
+module.exports = function(source) {
+  const content = marked(source)
+  return  `module.exports=\`${content}\``
+}
